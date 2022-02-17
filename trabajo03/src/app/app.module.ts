bdf121be512a78520928e5f28d3f9bc07879595e
Miguel Muñoz <jose.munoz01@epn.edu.ec>
2022-02-17 13:27:31 -0500

    Update: ruta-principal-component
    Adicion de la logica que ayuda a seleccionar la db

--- trabajo03/src/app/app.module.ts
+++ trabajo03/src/app/app.module.ts
@@ -11,6 +11,7 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {MatButtonModule} from "@angular/material/button";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import {MatButtonModule} from "@angular/material/button";
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- trabajo03/src/app/rutas/ruta-principal/ruta-principal.component.ts
+++ trabajo03/src/app/rutas/ruta-principal/ruta-principal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {SqlConexionService} from "../../servicios/http/sql-conexion.service";
 
 @Component({
   selector: 'app-ruta-principal',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutaPrincipalComponent implements OnInit {
 
-  constructor() { }
+  formGroup?:FormGroup
+  listaBases?:string[] = [];
+  baseElegida: string = '';
+  constructor(
+    private readonly formBuilder:FormBuilder,
+    private readonly apiService:SqlConexionService
+  ) { }
 
   ngOnInit(): void {
+    this.cargarBases()
+  }
+
+  cargarBases(){
+    this.apiService.buscarBases()
+      .subscribe({
+        next: (datos) => {
+          this.listaBases = datos.map((element)=>{
+            return element.name;
+          })
+          this.prepararFormulario()
+        }
+      })
+  }
+
+  prepararFormulario(){
+    this.formGroup = this.formBuilder.group({
+      listaDb: new FormControl('')
+    })
+  }
+
+  baseSeleccionada(){
+    const base = this.formGroup?.get('listaDb')
+    if(base){
+      this.baseElegida = base.value;
+    }
   }
 
 }
